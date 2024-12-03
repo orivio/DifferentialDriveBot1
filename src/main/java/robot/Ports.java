f3e0b4863342413727c034c0feefc52382c49732
Russlan <russlanhossain@gmail.com>
2024-12-02 19:39:58 -0800

    worked on differential drive up to odometry

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -6,4 +6,11 @@ public final class Ports {
     public static final int OPERATOR = 0;
     public static final int DRIVER = 1;
   }
+
+  public static final class Drive {
+    public static final int RIGHT_LEADER = 2;
+    public static final int RIGHT_FOLLOWER = 3;
+    // etc
+  }
+
 }
